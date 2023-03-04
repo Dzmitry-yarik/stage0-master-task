67d81c80e33ff3d1b930d1b30e763bea559ab38f
Dzmitry-yarik <107866389+Dzmitry-yarik@users.noreply.github.com>
2023-03-04 21:00:38 +0300

    Update CoordinatePane.java

--- src/main/java/com/epam/conditions/CoordinatePane.java
+++ src/main/java/com/epam/conditions/CoordinatePane.java
@@ -1,9 +1,18 @@
 package com.epam.conditions;
 
 public class CoordinatePane {
-
     public void printQuadrant(int x, int y) {
+        if (x == 0 && y == 0)
+            System.out.print("0");
+        else if (x == 0 || y == 0)
+            System.out.print("Coordinates belong to two quarters at the same time");
+        else if (x >= 0 && y >= 0)
+            System.out.print("1");
+        else if (x <= 0 && y >= 0)
+            System.out.print("2");
+        else if (x <= 0 && y <= 0)
+            System.out.print("3");
+        else if (x >= 0 && y <= 0)
+            System.out.print("4");
     }
-
 }
-
