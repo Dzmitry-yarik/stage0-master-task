9904f846d83f76707cf1caef90a0e9e9aa094f34
Dzmitry-yarik <107866389+Dzmitry-yarik@users.noreply.github.com>
2023-03-04 21:07:03 +0300

    Update Pyramid.java

--- src/main/java/com/epam/loops/Pyramid.java
+++ src/main/java/com/epam/loops/Pyramid.java
@@ -1,13 +1,26 @@
 package com.epam.loops;
 
 public class Pyramid {
-
     public void printPyramid(int cathetusLength) {
-
+        int x;
+        for (int i = 1; i <= cathetusLength; i++) {
+            x = i - 1;
+            for (int j = i; j <= cathetusLength - 1; j++) {
+                System.out.print(" ");
+                System.out.print("  ");
+            }
+            for (int j = 0; j <= x; j++)
+                System.out.print((i + j) < cathetusLength * 2 ? (i - j) + "  " : (i + j) + " ");
+            int a = 2;
+            for (int j = 1; j <= x; j++) {
+                System.out.print((i + x - j) < cathetusLength * 2 ? (a) + "  " : (i + x - j) + " ");
+                a++;
+            }
+            System.out.println();
+        }
     }
 
     public static void main(String[] args) {
-        new Pyramid().printPyramid(7);
+        new Pyramid().printPyramid(8);
     }
-
 }
