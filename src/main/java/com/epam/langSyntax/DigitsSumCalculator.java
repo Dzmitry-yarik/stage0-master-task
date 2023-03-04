302158c9905a93f3b6550d6dcef8d8aead175115
Dzmitry-yarik <107866389+Dzmitry-yarik@users.noreply.github.com>
2023-03-04 21:04:45 +0300

    Update DigitsSumCalculator.java

--- src/main/java/com/epam/langSyntax/DigitsSumCalculator.java
+++ src/main/java/com/epam/langSyntax/DigitsSumCalculator.java
@@ -1,8 +1,14 @@
 package com.epam.langSyntax;
 
 public class DigitsSumCalculator {
-
     public void calculateSum(int number) {
+        int sum = 0;
+        while (number > 0) {
+            sum = sum + number % 10; //Суммируем остаток от деления
+            number /= 10; //Отсекаем последнюю цифру
+
+        }
+        System.out.println("Сумма всех цифр = " + sum);
     }
 
 }
