9513a330cefe640f5f28b62a0dee321a3e6df939
Dzmitry-yarik <107866389+Dzmitry-yarik@users.noreply.github.com>
2023-03-04 21:03:01 +0300

    Update TriangleSidesValidator.java

--- src/main/java/com/epam/conditions/TriangleSidesValidator.java
+++ src/main/java/com/epam/conditions/TriangleSidesValidator.java
@@ -1,9 +1,11 @@
 package com.epam.conditions;
 
-public class TriangleSidesValidator {
-
+public class TriangleValidator {
     public void validate(double firstSide, double secondSide, double thirdSide) {
-
+         if ((firstSide + secondSide) > thirdSide &
+                 (secondSide + thirdSide) > firstSide & (thirdSide + firstSide) > secondSide) {
+             System.out.println("The triangle exist.");
+         } else {
+             System.out.println("The triangle doesn't exist."); }
     }
-
 }
