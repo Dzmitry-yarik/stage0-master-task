a446e1eccc0bede0da26630a400374d08d121927
Dzmitry-yarik <107866389+Dzmitry-yarik@users.noreply.github.com>
2023-03-05 00:08:47 +0300

    Update TriangleSidesValidator.java

--- src/main/java/com/epam/conditions/TriangleSidesValidator.java
+++ src/main/java/com/epam/conditions/TriangleSidesValidator.java
@@ -1,6 +1,6 @@
 package com.epam.conditions;
 
-public class TriangleValidator {
+public class TriangleSidesValidator {
     public void validate(double firstSide, double secondSide, double thirdSide) {
          if ((firstSide + secondSide) > thirdSide &
                  (secondSide + thirdSide) > firstSide & (thirdSide + firstSide) > secondSide) {
