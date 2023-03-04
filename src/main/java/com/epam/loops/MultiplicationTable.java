ddaa1b081e0dda0050ca16343b3abe1820a455aa
Dzmitry-yarik <107866389+Dzmitry-yarik@users.noreply.github.com>
2023-03-04 21:06:29 +0300

    Update MultiplicationTable.java

--- src/main/java/com/epam/loops/MultiplicationTable.java
+++ src/main/java/com/epam/loops/MultiplicationTable.java
@@ -1,8 +1,16 @@
 package com.epam.loops;
 
 public class MultiplicationTable {
-
     public void printTable(int numberTableToPrint) {
+        for(int i = 1; i <= 10; i++){
+            System.out.println(numberTableToPrint + " * " + i + " = " + numberTableToPrint * i);
+        }
+        System.out.println("***********************************************");
+        for(int i = 1; i <= numberTableToPrint; i++){
+            for(int k = 1; k <= 10; k++){
+                System.out.print(k * i + "\t");
+            }
+            System.out.println("");
+        }
     }
-
 }
