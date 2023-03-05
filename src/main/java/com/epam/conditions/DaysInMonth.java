4950d5dab9441569fe0d08221b0c82aa0d7479b9
Dzmitry-yarik <107866389+Dzmitry-yarik@users.noreply.github.com>
2023-03-05 13:01:54 +0300

    Update DaysInMonth.java

--- src/main/java/com/epam/conditions/DaysInMonth.java
+++ src/main/java/com/epam/conditions/DaysInMonth.java
@@ -1,34 +1,58 @@
 package com.epam.conditions;
 
-public class DaysInMonth {
-
     public void printDays(int year, int month) {
-        if (month <= 0 || month > 12){
+        if (month <= 0 || month > 12) {
+            System.err.println("invalid date");
             return;
         }
-        switch(month)
-        {case 1: System.out.println("31");break;
-            case 2:  if (year %100==0 && year %400 ==0 )
-                System.out.println("29");
+        switch (month) {
+            case 1:
+                System.err.println("31");
+                break;
+            case 2:
+                if (year % 100 == 0 && year % 400 == 0)
+                    System.err.println("29");
+                else if (year % 4 == 0 && year % 100 > 0)
+                    System.err.println("29");
+                else if (year % 100 == 0)
+                    System.err.println("28");
+                else
+                    System.err.println("28");
+                break;
+            case 3:
+                System.err.println("31");
+                break;
+            case 4:
+                System.err.println("30");
+                break;
+            case 5:
+                System.err.println("31");
+                break;
+            case 6:
+                System.err.println("30");
+                break;
+            case 7:
+                System.err.println("31");
+                break;
+            case 8:
+                System.err.println("31");
+                break;
+            case 9:
+                System.err.println("30");
+                break;
+            case 10:
+                System.err.println("31");
+                break;
+            case 11:
+                System.err.println("30");
+                break;
+            case 12:
+                System.err.println("31");
+                break;
+            default:
+                System.err.println("Error");
+                break;
+        }
 
-            else if (year % 4 == 0 && year % 100 > 0)
-                System.out.println("29");
-
-            else if (year % 100 == 0)
-                System.out.println("28");
-
-            else
-                System.out.println("28");
-               break;
-            case 3: System.out.println("31");break;
-            case 4: System.out.println("30");break;
-            case 5: System.out.println("31");break;
-            case 6: System.out.println("30");break;
-            case 7: System.out.println("31");break;
-            case 8: System.out.println("31");break;
-            case 9: System.out.println("30");break;
-            case 10: System.out.println("31");break;
-            case 11: System.out.println("30");break;
-            case 12: System.out.println("31");break;}
     }
 }
