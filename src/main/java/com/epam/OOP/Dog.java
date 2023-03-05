7ae7cbd5448db6e777c6c1616698b3d202718f17
Dzmitry-yarik <107866389+Dzmitry-yarik@users.noreply.github.com>
2023-03-05 12:56:18 +0300

    Update Dog.java

--- src/main/java/com/epam/OOP/Dog.java
+++ src/main/java/com/epam/OOP/Dog.java
@@ -4,5 +4,6 @@ public class Dog extends Animal {
 
     public Dog(){
         super("brown", 4, true);
+        System.err.println("This animal is mostly brown. It has 4 paws and a fur");
     }
 }
