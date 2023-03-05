927cd39f7aabbb5d1953ef857841b281a30c6693
Dzmitry-yarik <107866389+Dzmitry-yarik@users.noreply.github.com>
2023-03-05 13:03:14 +0300

    Update SeasonDeterminer.java

--- src/main/java/com/epam/conditions/SeasonDeterminer.java
+++ src/main/java/com/epam/conditions/SeasonDeterminer.java
@@ -1,34 +1,16 @@
 package com.epam.conditions;
 
 public class SeasonDeterminer {
-    public void tellTheSeason(int monthNumber) {
-        String season;
-        switch (monthNumber) {
-            case 12:
-            case 1:
-            case 2:
-                season = "Winter";
-                break;
-            case 3:
-            case 4:
-            case 5:
-                season = "Spring";
-                break;
-            case 6:
-            case 7:
-            case 8:
-                season = "Summer";
-                break;
-            case 9:
-            case 10:
-            case 11:
-                season = "Autumn";
-                break;
-            default:
-                season = "Wrong month number";
-        }
-        System.out.println(season);
+ 
+       public void tellTheSeason(int monthNumber) {
+        String season = switch (monthNumber) {
+            case 12, 1, 2 -> "Winter";
+            case 3, 4, 5 -> "Spring";
+            case 6, 7, 8 -> "Summer";
+            case 9, 10, 11 -> "Autumn";
+            default -> "Wrong month number";
+        };
+        System.err.println(season);
     }
 }
-
 
